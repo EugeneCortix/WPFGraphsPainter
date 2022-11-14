fdf2afdcb758800e980a49e0e3b04feef6126933
kyleerod <kyleerod@student.21-school.ru>
2022-11-14 21:43:32 +0700

    image

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -56,14 +56,23 @@ namespace WpfApp1
         double xmin = 0;
         double ymin = 0;
 
+        DrawingGroup drawingGroup = new DrawingGroup();
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
 
+
+
+
             dataValuesObject = new ObservableCollection<dataValues> 
             { 
             new dataValues{X = 0,Y = 0},
-            new dataValues{X = 1,Y = 2}
+            new dataValues{X = 1,Y = 1}
             };
             dataValuesGrid.ItemsSource = dataValuesObject;
 
@@ -90,11 +99,11 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            dataValuesObject.Add(new dataValues { X = 4, Y = 16 });
+            double xpr = dataValuesObject[dataValuesObject.Count - 1].X + 1;
+            double ypr = xpr*xpr;
+            dataValuesObject.Add(new dataValues { X = xpr, Y = ypr });
             repaint();
         }
-
-        
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -153,7 +162,6 @@ namespace WpfApp1
 
         }
 
-
         private ObservableCollection<dataValues> readData(string filename)
         {
 
@@ -182,6 +190,11 @@ namespace WpfApp1
 
         private void drawLine(string g)
         {
+            GeometryDrawing myGeometryDrawing = new GeometryDrawing();
+            GeometryGroup lines = new GeometryGroup();
+            myGeometryDrawing.Pen = new Pen(colorPens[g], 3);
+            
+
             for (int i = 1; i < _data[g].Count; i++)
             {
                 // System.Diagnostics.Debugger.Break();
@@ -191,56 +204,80 @@ namespace WpfApp1
                 double y2 = (_data[g][i].Y - ySubZero) * ky;
 
 
-                Line newline = new Line()
-                {
-                    X1 = x1,
-                    Y1 = 300 - y1,
-                    X2 = x2,
-                    Y2 = 300 - y2,
-                    Stroke = colorPens[g],
-                    StrokeThickness = 3
-                };
-                graphCanvas.Children.Add(newline);
+              
+
+
+                lines.Children.Add(new LineGeometry(new Point(x1, graphImage.Height - y1), new Point(x2, graphImage.Height - y2)));
             }
+
+            myGeometryDrawing.Geometry = lines;
+
+            drawingGroup.Children.Add(myGeometryDrawing);
+
+            graphImage.Source = new DrawingImage(drawingGroup);
         }
 
         private void drawSline (string g)
         {
+            GeometryDrawing myGeometryDrawing = new GeometryDrawing();
+            GeometryGroup splines = new GeometryGroup();
+            myGeometryDrawing.Pen = new Pen(colorPens[g], 3);
             if (_data[g].Count < 3)
             {
                 drawLine(g);
                 return;
             }
-            Path spline = new Path();
-            spline.Stroke  = colorPens[g];
-            spline.StrokeThickness = 3;
-            PathGeometry geometry = new PathGeometry();
-            PathFigure figure = new PathFigure();
-            figure.StartPoint = new Point((_data[g][0].X - xSubZero) * kx, 
-                300 - (_data[g][0].Y - ySubZero) * ky);
 
-            PointCollection p = new PointCollection();
-            for (int i = 1; i < _data[g].Count; i ++)
+            for(int i = 1; i < _data[g].Count; i++)
             {
-                p.Add(new Point((_data[g][i].X - xSubZero) * kx,
-                   300 - (_data[g][i].Y - ySubZero) * ky));
-            }
-            PolyQuadraticBezierSegment myBezierSegment = new PolyQuadraticBezierSegment();
-            myBezierSegment.Points = p;
-            figure.Segments.Add(myBezierSegment);
+                Point startPoint = new Point((_data[g][i -1 ].X - xSubZero) * kx,
+                   300 - (_data[g][i - 1].Y - ySubZero) * ky);
 
-            geometry.Figures.Add(figure);
-            spline.Data = geometry;
-            graphCanvas.Children.Add(spline);
+                double dx = (_data[g][i].X - _data[g][i - 1].X) * (1.0 / 3.1);
+
+                Point p1 = new Point((_data[g][i - 1].X  - xSubZero + dx) * kx ,
+                   300 - (_data[g][i - 1].Y - ySubZero) * ky);
+
+                Point p2 = new Point((_data[g][i].X  - xSubZero - dx) * kx ,
+                                    300 - (_data[g][i].Y  - ySubZero) * ky);
+
+                Point endPoint = new Point((_data[g][i].X - xSubZero) * kx,
+                                    300 - (_data[g][i].Y - ySubZero) * ky);
+                BezierSegment bz = new BezierSegment();
+                bz.Point1 = p1;
+                bz.Point2 = p2;
+                bz.Point3 = endPoint;
+                PathFigure pf = new PathFigure();
+                pf.Segments.Add(bz);
+                PathGeometry pg = new PathGeometry();
+                pf.StartPoint = startPoint;
+                pg.Figures.Add(pf);
+                splines.Children.Add(pg);
+            }
+
+            //PathFigure pf = new PathFigure();
+            //  pf.Segments.Add(myBezierSegment);
+            //  PathGeometry pg = new PathGeometry();
+            //  pf.StartPoint = new Point((_data[g][0].X - xSubZero) * kx,
+          /*  300 - (_data[g][0].Y - ySubZero) * ky);
+            pg.Figures.Add(pf);
+            splines.Children.Add(pg);*/
+            myGeometryDrawing.Geometry = splines;
+
+            drawingGroup.Children.Add(myGeometryDrawing);
+            graphImage.Source = new DrawingImage(drawingGroup);
         }
+
 
         private void paint(string g)
         {
             draw(g);
+
             Label leglab = new Label();
             leglab.Content = g;
             leglab.Background = (colorPens[g]);
             stackLegend.Children.Add(leglab);
+            zoomPic();
             
         }
 
@@ -268,8 +305,8 @@ namespace WpfApp1
             if (xmax - xmin > xScale) xScale = xmax - xmin;
             if (ymax - ymin > yScale) yScale = ymax - ymin;
 
-            kx = graphCanvas.ActualWidth / xScale;
-            ky = graphCanvas.ActualHeight / yScale;
+            kx = graphImage.Width / xScale;
+            ky = graphImage.Height / yScale;
 
             if (xmin < xSubZero) xSubZero = xmin;
             if (ymin < ySubZero) ySubZero = ymin;
@@ -277,47 +314,40 @@ namespace WpfApp1
 
         private void makegrid()
         {
+            GeometryDrawing myGeometryDrawing = new GeometryDrawing();
+            GeometryGroup lines = new GeometryGroup();
+            myGeometryDrawing.Pen = new Pen(Brushes.LightGray, 1);
+
             for (int i = 0; i <= xScale; i++)
             {
-                Line newline = new Line()
-                {
-                    X1 = i*kx,
-                    Y1 = 300,
-                    X2 = i*kx,
-                    Y2 = 0,
-                    Stroke = Brushes.LightGray,
-                    StrokeThickness = 1
-                };
-                graphCanvas.Children.Add(newline);
+
+                lines.Children.Add(new LineGeometry(new Point(i * kx, graphImage.Height), new Point(i * kx, 0)));
             }
             for (int i = 0; i <= yScale; i++)
             {
-                Line newline = new Line()
-                {
-                    X1 = 0,
-                    Y1 = i*ky,
-                    X2 = 400,
-                    Y2 = i*ky,
-                    Stroke = Brushes.LightGray,
-                    StrokeThickness = 1
-                };
-                graphCanvas.Children.Add(newline);
+
+                lines.Children.Add(new LineGeometry(new Point(0, i * ky), new Point(graphImage.Width, i * ky)));
             }
 
+            myGeometryDrawing.Geometry = lines;
+
+            drawingGroup.Children.Add(myGeometryDrawing);
+
+
+            graphImage.Source = new DrawingImage(drawingGroup);
+            overlay();
             // Labels
-            if(zoom == 1)
+            if (zoom == 1)
             {
                 labelxl.Content = (xmin).ToString();
-                labelxm.Content = ((xmax - xmin) / 2).ToString();
+                labelxm.Content = ((xmax + xmin) / 2).ToString();
                 labelxr.Content = (xmax).ToString();
                 labelyb.Content = (ymin).ToString();
-                labelym.Content = ((ymax - ymin) / 2).ToString();
+                labelym.Content = ((ymax + ymin) / 2).ToString();
                 labelyt.Content = (ymax).ToString();
                 overCanvas.Children.Clear();
             }
         }
-
-       // private void
 
         private void ListOfFiles_MouseMove(object sender, MouseEventArgs e)
         {
@@ -326,11 +356,14 @@ namespace WpfApp1
 
         private void repaint()
         {
+            
             var items = listOfFiles.Items;
 
-            graphCanvas.Children.Clear();
-            stackLegend.Children.Clear();
+            drawingGroup.Children.Clear();
+            graphImage.Source = new DrawingImage(drawingGroup);
 
+            stackLegend.Children.Clear();
+            
             xScale = 0;
             yScale = 0;
             xSubZero = 0;
@@ -349,7 +382,6 @@ namespace WpfApp1
                 };
             }
             makegrid();
-
             foreach (CheckBox c in checkBoxes)
             {
                 if (c.IsChecked == true)
@@ -358,6 +390,91 @@ namespace WpfApp1
                     paint(s);
                 };
             }
+            GeometryDrawing ar = new GeometryDrawing();
+
+            ar.Brush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            ar.Pen = new Pen(ar.Brush, 3);
+            ar.Geometry = new RectangleGeometry(new Rect(new Size(400 * zoom, 300 * zoom)));
+
+            drawingGroup.Children.Add(ar);
+        }
+        
+
+        private void StackLegend_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //ChooseColor(sender, e);
+
+            //25
+            double h = e.GetPosition(stackLegend).Y;
+            int i = Convert.ToInt32(Math.Floor(h / 25.0));
+            string s = (((sender as StackPanel)).Children[i] as Label)?.Content?.ToString();
+            //System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
+            ColorDialog colorDialog = new ColorDialog();
+
+                colorDialog.ShowDialog();
+            //colorDialog.ShowDialog.DialogResult;
+            
+            System.Drawing.Color color1 = colorDialog.Color;
+            /*if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;*/
+            Color color = Colors.Red;
+            color.A = color1.A;
+            color.R = color1.R;
+            color.G = color1.G;
+            color.B = color1.B;
+            Brush brush = new SolidColorBrush(color);
+            colorPens[s] = brush;
+            ((sender as StackPanel).Children[i] as Label).Background = (colorPens[s]);
+            // draw(s);
+            repaint();
+        }
+
+        private void DrawWay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            repaint();
+        }
+
+        private void graphImage_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            zoom += zoomSpeed * e.Delta; // Ajust zooming speed (e.Delta = Mouse spin value )
+            if (zoom < zoomMin) { zoom = zoomMin; } // Limit Min Scale
+            if (zoom > zoomMax) { zoom = zoomMax; } // Limit Max Scale
+
+
+            labelxl.Content = "";
+            labelxm.Content = "";
+            labelxr.Content = "";
+            labelyb.Content = "";
+            labelym.Content = "";
+            labelyt.Content = "";
+
+            ListOfFiles_MouseMove(sender, e);
+
+        }
+
+        private void zoomPic()
+        {
+            Point mousePos = Mouse.GetPosition(graphImage);
+
+            //drawingGroup.Transform = new ScaleTransform(zoom, zoom, mousePos.X, mousePos.Y);
+
+            // drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(new Size(graphImage.Width, graphImage.Height)));
+
+
+
+            if (zoom > 1)
+            {
+                //drawingGroup.Transform = new ScaleTransform(1/zoom, 1/zoom, mousePos.X, mousePos.Y);
+
+                graphImage.RenderTransform = new ScaleTransform(zoom, zoom, mousePos.X, mousePos.Y); // transform Canvas size from mouse position
+            }
+            else
+            {
+                //drawingGroup.Transform = new ScaleTransform(zoom, zoom);
+                graphImage.RenderTransform = new ScaleTransform(zoom, zoom); // transform Canvas size
+            }
+
+              graphImage.Source = new DrawingImage(drawingGroup);
         }
 
         private void overlay()
@@ -369,7 +486,7 @@ namespace WpfApp1
             Rectangle rect4 = new Rectangle();
 
             Brush wBrush = new SolidColorBrush(Colors.White);
-
+            
             // Left
             rect1.Width = 256;
             rect1.Height = 420;
@@ -388,83 +505,24 @@ namespace WpfApp1
             //устанавливаем расположение
             Canvas.SetTop(rect2, 0);
             Canvas.SetLeft(rect2, 0);
-
+            
             // Bottom
-            rect3.Width = 906;
-            rect3.Height = 420-346;
+            rect3.Width = window.Width;
+            rect3.Height = window.Height - 46 - graphImage.Height;
             rect3.Fill = wBrush;
             overCanvas.Children.Add(rect3);
             //устанавливаем расположение
-            Canvas.SetTop(rect3, 346);
+            Canvas.SetTop(rect3, graphImage.Height+46);
             Canvas.SetLeft(rect3, 0);
 
             // Right
-            rect4.Width = 906-(256 + 403);
+            rect4.Width = window.Width - (256 + 403);
             rect4.Height = 420;
             rect4.Fill = wBrush;
             overCanvas.Children.Add(rect4);
             //устанавливаем расположение
-            Canvas.SetTop(rect3, 0);
-            Canvas.SetLeft(rect3, 256+403);
-        }
-
-        private void GraphCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-             zoom += zoomSpeed * e.Delta; // Ajust zooming speed (e.Delta = Mouse spin value )
-             if (zoom < zoomMin) { zoom = zoomMin; } // Limit Min Scale
-             if (zoom > zoomMax) { zoom = zoomMax; } // Limit Max Scale
-
-             Point mousePos = e.GetPosition(graphCanvas);
-
-
-             if (zoom > 1)
-             {
-                 graphCanvas.RenderTransform = new ScaleTransform(zoom, zoom, mousePos.X, mousePos.Y); // transform Canvas size from mouse position
-                overlay(); // Hide extra
-            }
-             else
-             {
-                 graphCanvas.RenderTransform = new ScaleTransform(zoom, zoom); // transform Canvas size
-             }
-
-                labelxl.Content = "";
-                labelxm.Content = "";
-                labelxr.Content = "";
-                labelyb.Content = "";
-                labelym.Content = "";
-                labelyt.Content = "";
-
-            // graphCanvas.Width = 400;
-            // graphCanvas.Height = 300;
-          //  overlay();
-            ListOfFiles_MouseMove(sender, e);
-        }
-
-        private void StackLegend_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            //ChooseColor(sender, e);
-
-            //25
-            double h = e.GetPosition(stackLegend).Y;
-            int i = Convert.ToInt32(Math.Floor(h / 25.0));
-            string s = (((sender as StackPanel)).Children[i] as Label)?.Content?.ToString();
-            System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
-            colorDialog.ShowDialog();
-            System.Drawing.Color color1 = colorDialog.Color;
-            Color color = Colors.Red;
-            color.A = color1.A;
-            color.R = color1.R;
-            color.G = color1.G;
-            color.B = color1.B;
-            Brush brush = new SolidColorBrush(color);
-            colorPens[s] = brush;
-            ((sender as StackPanel).Children[i] as Label).Background = (colorPens[s]);
-            draw(s);
-        }
-
-        private void DrawWay_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            repaint();
+            Canvas.SetTop(rect4, 0);
+            Canvas.SetLeft(rect4, 252 + graphImage.Width);
         }
     }
     public class dataValues
